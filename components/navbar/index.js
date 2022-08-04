041c8d518f30c300a1fb2feba1c39d52633b0b4d
Arbaz Khan <arbazkhangul123@gmail.com>
2022-08-04 11:49:06 +0500

    addgf

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import Link from 'next/link'
+import { useState } from "react";
 import   {AiOutlineMenu} from "react-icons/ai"
 const Navbar=()=>{
+  const [showItems, show]=useState(false);
 
     return(
         <>
-        <div className="navbar flex items-center pt-[1rem] pb-[1rem] pl-[1rem] pr-[2rem] md:pl-[4rem] md:pr-[5rem] md:pt-[0rem] md:pb-[0rem]" >
+        <div className="navbar flex items-center pt-[1rem] pb-[1rem] pl-[1rem] pr-[2rem]  sm:pl-[2rem] sm:pr-[3rem] md:pl-[4rem] md:pr-[5rem] md:pt-[0rem] md:pb-[0rem]" >
 
-        <div className=" w-[26rem] h-[4.2rem] md:w-[35rem] md:h-[6rem]  relative">
+        <div className=" w-[26rem] h-[4.2rem] sm:w-[30rem] sm:h-[5rem]  md:w-[35rem] md:h-[6rem]  relative">
   <Image 
   src="/logo.png"
   layout="fill"
@@ -17,7 +19,12 @@ const Navbar=()=>{
   
   <div className="flex-1 items-center flex">
 
-  <AiOutlineMenu className="w-[2.7rem] ml-auto h-[2.7rem]  iconvisibility text-white"></AiOutlineMenu>
+  <AiOutlineMenu className="w-[2.7rem] ml-auto h-[2.7rem]  inline-block md:hidden text-white"></AiOutlineMenu>
+
+
+
+
+
 
     <ul className="inline-block ml-auto list">
       <li className="inline-block links">
@@ -48,6 +55,38 @@ const Navbar=()=>{
     </ul>
 
   </div> 
+</div>
+
+<div className="onclicklist">
+
+    
+  <ul className=" ml-auto divide-y-[1px] divide-[#454f5a]">
+      <li className="links p-[0.3rem]">
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <li className=" onlinks p-[0.3rem]">
+        <Link href="/about">
+          <a>NFTs</a>
+        </Link>
+      </li>
+      <li className=" onlinks p-[0.3rem]">
+        <Link href="/blog/hello-world">
+          <a>Collections</a>
+        </Link>
+      </li>
+      <li className="onlinks p-[0.3rem]">
+        <Link href="/blog/hello-world">
+          <a>Login</a>
+        </Link>
+      </li>
+      <li className="onlinks p-[0.3rem]">
+        <Link href="/blog/hello-world">
+          <a>Register</a>
+        </Link>
+      </li>
+    </ul>
 </div>
         </>
     )
