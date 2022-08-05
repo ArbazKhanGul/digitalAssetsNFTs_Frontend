755a7ac99d1dda787cec881fd63a82208330e6ca
Arbaz Khan <arbazkhangul123@gmail.com>
2022-08-05 08:10:14 +0500

    dabdfa

--- components/mainpage/index.js
+++ components/mainpage/index.js
@@ -3,11 +3,11 @@ import Image from "next/image";
 const Main=()=>{
 
     return (
-        <div className="main  overflow-x-hidden flex justify-evenly flex-col-reverse  md:flex-row items-center px-[5rem] ">
+        <div className="main  overflow-x-hidden flex justify-evenly flex-col md:flex-row items-center px-[5rem] ">
 
 <div className="text-white w-full md:w-1/2">
 
-    <h2 className="text-[3rem] md:text-[3.6rem] text-center text-[rgb(217,217,217)] mb-10">Discover And sell your golden words and thoughts</h2>
+    <h2 className="text-[3.2rem] md:text-[3.6rem] text-center text-[rgb(217,217,217)] mb-10">Discover And sell your golden words and thoughts</h2>
     <p className="text-[#DAD7D7] text-[1.7rem] md:text-[1.9rem] text-center mb-10">Goldernwords is world first NFT platform in which you can create NFTs of your golden words</p>
     <div className="text-[2rem] ml-8 text-center">
         <button>Login</button>
