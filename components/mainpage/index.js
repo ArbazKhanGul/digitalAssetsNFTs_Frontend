1cf57a39a83050910e49a93ef67308fbd219133a
Arbaz Khan <arbazkhangul123@gmail.com>
2022-08-05 08:12:04 +0500

    dabdfaj

--- components/mainpage/index.js
+++ components/mainpage/index.js
@@ -7,8 +7,8 @@ const Main=()=>{
 
 <div className="text-white w-full md:w-1/2">
 
-    <h2 className="text-[3.2rem] md:text-[3.6rem] text-center text-[rgb(217,217,217)] mb-10">Discover And sell your golden words and thoughts</h2>
-    <p className="text-[#DAD7D7] text-[1.7rem] md:text-[1.9rem] text-center mb-10">Goldernwords is world first NFT platform in which you can create NFTs of your golden words</p>
+    <h2 className="text-[2.8rem] sm:text-[3.2rem] md:text-[3.6rem] text-center text-[rgb(217,217,217)] mb-10">Discover And sell your golden words and thoughts</h2>
+    <p className="text-[#DAD7D7] text-[1.7rem] sm:text-[1.8rem] md:text-[1.9rem] text-center mb-10">Goldernwords is world first NFT platform in which you can create NFTs of your golden words</p>
     <div className="text-[2rem] ml-8 text-center">
         <button>Login</button>
         <button>Register</button>
