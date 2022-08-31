ab36b2cd45bbf08512a5c0969dbde0405f6ccae3
Arbaz Khan <arbazkhangul123@gmail.com>
2022-08-31 11:20:46 +0500

    final

--- pages/individualnft.js
+++ pages/individualnft.js
@@ -38,8 +38,8 @@ console.log(router.pathname)
         <meta property="og:title" content="The Rock arb arbaz" />
         <meta property="description"  content="How much does culture influence creative thinking?" key="og-desc"/>
         <meta property="og:description"  content="How much does culture influence creative thinking?" key="og-desc"/>
-        <meta property="image" content="https://textnft.vercel.app/nft.png" />
-        <meta property="og:image" content="https://textnft.vercel.app/nft.png" />
+        <meta property="image" content="https://textnft.vercel.app/nft.jpg" />
+        <meta property="og:image" content="https://textnft.vercel.app/nft.jpg" />
         {/* <meta property="og:image:width" content="467" /> */}
         {/* <meta property="og:image:height" content="88" /> */}
         <meta property="image:width" content="467" />
