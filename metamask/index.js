148d23283347a76f20311a3ec79ce9f0013c5df9
Arbaz Khan <arbazkhangul123@gmail.com>
2022-09-05 11:29:46 +0500

    ethereum

--- metamask/index.js
+++ metamask/index.js
@@ -16,7 +16,7 @@ export const connectWallet = async (dispatch,address) => {
       {
 
         const a = document.createElement("a");
-        a.href = "metamask://dapp/textnft.vercel.app";
+        a.href = "metamask://dapp://textnft.vercel.app";
         a.target = "_self";
         document.body.appendChild(a);
         a.click();
