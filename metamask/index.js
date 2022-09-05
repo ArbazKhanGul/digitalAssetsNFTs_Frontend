0bf0d0261a3104061554696d3c88d729bbc3a4e2
Arbaz Khan <arbazkhangul123@gmail.com>
2022-09-05 10:17:31 +0500

    ethereum

--- metamask/index.js
+++ metamask/index.js
@@ -9,18 +9,21 @@ export const connectWallet = async (dispatch,address) => {
   
     try {
 
-      if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-        window.open("https://metamask.app.link/dapp/textnft.vercel.app/");
-        return;
-        }    
+        
 
 
       if (!window?.ethereum || !window?.ethereum?.isMetaMask)
+      {
+
+        if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+          window.open("https://metamask.app.link/dapp/textnft.vercel.app/");
+        return;  
+        } 
       return toast.error("Please install MetaMask.", {
         position: "top-center",
       });
 
-
+    }
         const accountsFirst = await ethereum?.request({
           method: "eth_requestAccounts",
         });
