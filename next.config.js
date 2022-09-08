45c2fa7038a6d285972e5b27bb5d842efd255bd2
Arbaz Khan <arbazkhangul123@gmail.com>
2022-09-08 15:07:09 +0500

    final

--- next.config.js
+++ next.config.js
@@ -5,13 +5,13 @@ const nextConfig = {
   env: {
     CONTACT: "contact@gmailgoldennft.com",
     URL: 'https://textnft.vercel.app',
-    SERVER:'http://localhost:5000/images',
-    // SERVER:'https://24c6-119-160-64-218.ngrok.io/images',
-    SERVER_URL: 'http://localhost:5000'
-    // SERVER_URL:'https://24c6-119-160-64-218.ngrok.io'
+    // SERVER:'http://localhost:5000/images',
+    SERVER:'https://3a89-119-160-64-216.ngrok.io/images',
+    // SERVER_URL: 'http://localhost:5000'
+    SERVER_URL:'https://3a89-119-160-64-216.ngrok.io'
   },
   images: {
-    domains: ['localhost','24c6-119-160-64-218.ngrok.io'],
+    domains: ['localhost','3a89-119-160-64-216.ngrok.io'],
 },
 }
 
