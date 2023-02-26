12175b10c4b9a2ce27d8a08cd1fda672c187a556
Arbaz Khan <arbazkhangul123@gmail.com>
2023-02-26 21:17:42 +0500

    server_url

--- utils/axiosconfiguration.js
+++ utils/axiosconfiguration.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const API=axios.create({
     baseURL: process.env.SERVER_URL,
     withCredentials: true,
-    credentials: 'include',
-    crossDomain: true,
+    // credentials: 'include',
+    // crossDomain: true,
 })
 
 export default API;
