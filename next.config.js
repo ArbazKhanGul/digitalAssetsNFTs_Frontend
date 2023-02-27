56ddba518c3362c2430e55fe1d64cdac4fc14152
Arbaz Khan <arbazkhangul123@gmail.com>
2023-02-27 07:17:58 +0500

    server_url

--- next.config.js
+++ next.config.js
@@ -11,11 +11,11 @@ const nextConfig = {
     // SERVER:'http://localhost:5000/images',
     // SERVER:'https://3a89-119-160-64-216.ngrok.io/images',
     // SERVER_URL: 'http://localhost:5000',
-    SERVER_URL:'https://grumpy-schools-study-119-160-67-12.loca.lt',
+    SERVER_URL:'https://34f8-119-160-67-12.ngrok.io',
     chainId:31337
   },
   images: {
-    domains: ['localhost','grumpy-schools-study-119-160-67-12.loca.lt'],
+    domains: ['localhost','34f8-119-160-67-12.ngrok.io'],
 },
 }
 
