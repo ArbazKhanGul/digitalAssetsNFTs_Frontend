3634f80e51e562940bae89b899998bd670434c09
Arbaz Khan <arbazkhangul123@gmail.com>
2023-10-08 10:23:54 +0500

    copyright_complete

--- components/meta/index.js
+++ components/meta/index.js
@@ -23,12 +23,18 @@ const Meta=({nftData,contentData}) => {
        }
 
 
-       {contentData.type=="video" && <>
-        <meta property="description"  content={`${contentData?.description}`}/>
-        <meta property="og:description"  content={`${contentData?.description}`} /> 
-        <meta property="og:video" content={`${process.env.ipfsURL}${contentData?.content}`} />
-        <meta property="og:video:secure_url" content={`${process.env.ipfsURL}${contentData?.content}`} />
-        {/* <meta property="og:image:type" content="image/png" /> */}
+       {contentData.type=="video" && 
+       <>
+<meta property="og:url" content="http://www.youtube.com/watch?v=CFLGRidfF04"/>
+<meta property="og:title" content="Vanilla Ice Ninja Rap - Go Ninja, Go Ninja Go!"/>
+<meta property="og:description" content="Ninja Rap music video by Vanilla Ice from Teenage Mutant Ninja Turtles 2 movie back in '91. Classic."/>
+<meta property="og:type" content="video"/>
+<meta property="og:image" content="http:///14.ytimg.com/vi/GFLGRidF04/default.jpg"/>
+<meta property="og:video" content="http://www.youtube.com/v/GFLGRidF04?version=3&autohide=1">/
+<meta property="og:video:type" content="application/x-shockwave-flash"/>
+<meta property="og:video:width" content="398"/>
+<meta property="og:video:height" content="264"/>
+<meta property="og:site_name" content="YouTube"/>
         </>
        }
 
