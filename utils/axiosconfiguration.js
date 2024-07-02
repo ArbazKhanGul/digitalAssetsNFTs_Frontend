01d28e4ff1ec402d36778c5dd69a4719dd1bc3fb
Arbaz Khan <arbazkhangul123@gmail.com>
2024-07-02 15:29:45 +0500

    jwt secret add

--- utils/axiosconfiguration.js
+++ utils/axiosconfiguration.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URL,
+  baseURL: 'https://digitalassetsnfts-backend.onrender.com',
   withCredentials: true,
 });
 
